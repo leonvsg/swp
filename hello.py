83050554dfec11e69b46cf3a3bf4e22a1f15cb68
leonvsg <leonvsg@gmail.com>
2016-03-06 21:55:31 +0300

    py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def app(env, start_respone):
     args = env['QUERY_STRING']
     args.replace("&", "\n")
-    start_respone("200_OK", [("Content-Type", "text/plain")])
+    start_respone("200 OK", [("Content-Type", "text/plain")])
     return iter([args])
