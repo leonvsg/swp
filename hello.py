c100cb2d2b2055f45afc5896f8a1dbfb7609bfca
leonvsg <leonvsg@gmail.com>
2016-03-06 22:48:01 +0300

    qwe

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
+bind = "0.0.0.0:8080"
+pythonpath = "/home/box/web"
+
 def app(environ, start_response):
     args = environ['QUERY_STRING']
     args.replace("&", "\n")
     start_response("200 OK", [("Content-Type", "text/plain")])
-    return args
+    return [args]
