0a22701a8bd2810a19c2cb34d181cd49df22cd9b
leonvsg <leonvsg@gmail.com>
2016-03-06 23:12:23 +0300

    all change

--- hello.py
+++ hello.py
@@ -5,4 +5,4 @@ def app(environ, start_response):
     args = environ['QUERY_STRING']
     args.replace("&", "\n")
     start_response("200 OK", [("Content-Type", "text/plain")])
-    return [args]
+    return iter([args])
