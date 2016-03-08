6adfbde632c73c360678e561ed9f4ab0a197369b
leonvsg <leonvsg@gmail.com>
2016-03-08 19:27:56 +0300

    change urls

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from qa.views import test
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', test()),
-    url(r'^signup/$', test()),
-    url(r'^ask/$', test()),
-    url(r'^popular/$', test()),
-    url(r'^new/$', test()),
-    url(r'^question/\d+/$', test())
+    url(r'^login/$', 'qa.views.test'),
+    url(r'^signup/$', 'qa.views.test'),
+    url(r'^ask/$', 'qa.views.test'),
+    url(r'^popular/$', 'qa.views.test'),
+    url(r'^new/$', 'qa.views.test'),
+    url(r'^question/\d+/$', 'qa.views.test')
 ]
