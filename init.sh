3622d9b115928671352d007c19c3384c5baad035
leonvsg <leonvsg@gmail.com>
2016-03-08 19:08:10 +0300

    init

--- init.sh
+++ init.sh
@@ -2,5 +2,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo cd ask
 sudo gunicorn -b 0.0.0.0:8000 ask.wsgi
