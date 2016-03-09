9a6c25d50238f20ea6be4887549dd74d01594698
leonvsg <leonvsg@gmail.com>
2016-03-09 22:31:31 +0300

    models change

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,11 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Question(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField()
     rating = models.IntegerField()
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, related_name='question_set_1')
     likes = models.ManyToManyField(User)
 
 class Answer(models.Model):
