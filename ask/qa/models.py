a9180af6a444da838e3c8a5e4f45e7f23db7f205
leonvsg <leonvsg@gmail.com>
2016-03-09 18:35:53 +0300

    mysql update

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField()
+    text = models.TextField()
+    added_at - models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User)
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
