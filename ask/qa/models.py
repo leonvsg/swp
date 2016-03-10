a10f24aa3e1ef292980fcf58d5bceaf7f9d7bc8d
leonvsg <leonvsg@gmail.com>
2016-03-10 23:44:37 +0300

    edit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField()
     author = models.ForeignKey(User, related_name='question_set_1')
     likes = models.ManyToManyField(User)
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ sudo ln -sf /home/box/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
 sudo mysql -uroot -e "CREATE DATABASE ask;"
+sudo python ask/manage.py validate
