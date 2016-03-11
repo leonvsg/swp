1a22dc5fe759d6099f1750566ae69852d1321955
leonvsg <leonvsg@gmail.com>
2016-03-11 16:00:13 +0300

    update router

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^login', views.test),
     url(r'^signup', views.test),
     url(r'^ask', views.test),
-    url(r'^get_popular', views.get_popular),
+    url(r'^popular', views.get_popular),
     url(r'^new', views.test),
     url(r'^question/(&P<id>\d+)/$', views.get_question),
 ]
