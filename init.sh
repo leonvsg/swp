d7a7584b9964cc8dfd826c9a9cdf80531d86a8b6
leonvsg <leonvsg@gmail.com>
2016-03-11 15:33:48 +0300

    update

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
 sudo mysql -uroot -e "CREATE DATABASE ask;"
+sudo python ask/manage.py validate
 sudo python ask/manage.py syncdb
